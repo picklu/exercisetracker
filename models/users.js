5c5b80a433b777d30a1303e08d18975454846987
Subrata Sarker <subrata_sarker@yahoo.com>
2019-11-23 09:36:02 +0900

    Remove log and count from users

--- models/users.js
+++ models/users.js
@@ -11,16 +11,7 @@ const usersSchema = new mongoose.Schema({
         type: String,
         unique: true,
         required: [true, 'The domain name is required']
-    },
-    count: {
-        type: Number,
-        default: function () {
-            return this.log.length;
-        }
-    },
-    log: [
-        { type: mongoose.Schema.Types.ObjectId, ref: 'exercise' }
-    ]
+    }
 });
 
 module.exports = mongoose.model('users', usersSchema);
